e16df28c74b21d4035c75a2c87940f1e4166a726
liandawidyasari <liandawidyasari1@gmail.com>
2020-10-05 10:11:26 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1> Hello World </h1>
+    <p>Do kyungsoo adalah main vocalis dari EXO. dia sekarang sedang menjalani keewajibannya sebagai warga negara korea dengan Wajib Militer dan akan segera selesai wajib militernya pada tanggal 25 Januari 2021. </p>
 </body>
 
 </html>
